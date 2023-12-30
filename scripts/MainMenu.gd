8d5e2d05a77a866cc8c5cfb812877c0e88c5e8f5
holgac <huseyinolgac@gmail.com>
2023-12-30 18:14:49 +0300

    tree thumbs

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  pass # Replace with function body.
+  if OS.get_name() == 'Web':
+    get_node('Exit').hide();
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
